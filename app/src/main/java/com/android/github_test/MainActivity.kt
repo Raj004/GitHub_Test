f3e4a835c46e509cbe06641335eb1f4cff843d7c
raj004 <rajshekhartiwari14@gmail.com>
2020-12-17 12:57:48 +0530

    second-2

--- app/src/main/java/com/android/github_test/MainActivity.kt
+++ app/src/main/java/com/android/github_test/MainActivity.kt
@@ -2,11 +2,17 @@ package com.android.github_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        click.setOnClickListener {
+            Toast.makeText(this, "You are doing this in the right order!", Toast.LENGTH_LONG).show();
+
+        }
     }
 }
